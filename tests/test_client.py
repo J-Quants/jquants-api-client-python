26d469178f16f88f78560a41802bfa5f03fb05a3
Daito AKIMURA <dakimura@mercari.com>
2022-07-31 15:50:28 +0900

    introduce pytest-cov

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,7 @@
-def test_get_price_range():
+from src.client import JQuantsAPIClient
+
+
+def test_jquants_api_client():
+    cli = JQuantsAPIClient(refresh_token="hello")
     print("Hello world")
     assert True
