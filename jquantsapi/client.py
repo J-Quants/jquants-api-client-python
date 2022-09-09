4d64e98b694d824a69418006dc884e23673c24cd
hiohiohio <hiohiohio@users.noreply.github.com>
2022-09-09 16:04:05 +0900

    feat: support auth_ user API endpoint (#24)
    * feat: support auth_ user API endpoint
    
    * fix: resolve black and isort conflict
    
    * fix: fix patch in unittest
    
    * fix: drop parenthesized-context-managers
    
    * fix: add parameter validation

--- jquantsapi/client.py
+++ jquantsapi/client.py
@@ -1,15 +1,35 @@
 import os
+import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
-from typing import Dict, List, Optional, Union
+from pathlib import Path
+from typing import Any, List, Mapping, Optional, Union
 
 import pandas as pd  # type: ignore
 import requests
 from dateutil import tz
 from requests.adapters import HTTPAdapter
+from requests.exceptions import HTTPError
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 from urllib3.util import Retry
 
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
+
+
 DatetimeLike = Union[datetime, pd.Timestamp, str]
+_Data = Union[str, Mapping[str, Any]]
+
+
+class TokenAuthRefreshBadRequestException(Exception):
+    pass
 
 
 class Client:
@@ -21,28 +41,135 @@ class Client:
     JQUANTS_API_BASE = "https://api.jpx-jquants.com/v1"
     MAX_WORKERS = 5
 
-    def __init__(self, refresh_token: str) -> None:
+    def __init__(
+        self,
+        refresh_token: Optional[str] = None,
+        *,
+        mail_address: Optional[str] = None,
+        password: Optional[str] = None,
+    ) -> None:
         """
         Args:
-            refresh_token: J-Quants API リフレッシュトークン
+            refresh_token: J-Quants API refresh token
+            refresh_token_expiredat: refresh token expired_at
+            mail_address: J-Quants API login email address
+            password: J-Quants API login password
         """
-        self.refresh_token = refresh_token
+        config = self._load_config()
+
+        self._mail_address = config["mail_address"]
+        if mail_address is not None:
+            self._mail_address = mail_address
+
+        self._password = config["password"]
+        if password is not None:
+            self._password = password
+
+        self._refresh_token = config["refresh_token"]
+        if refresh_token is not None:
+            self._refresh_token = refresh_token
+
+        if self._refresh_token != "":
+            self._refresh_token_expire = pd.Timestamp.utcnow() + pd.Timedelta(
+                6, unit="D"
+            )
+        else:
+            self._refresh_token_expire = pd.Timestamp.utcnow()
+
         self._id_token = ""
         self._id_token_expire = pd.Timestamp.utcnow()
         self._session: Optional[requests.Session] = None
+
+        if ((self._mail_address == "") or (self._password == "")) and (
+            self._refresh_token == ""
+        ):
+            raise ValueError(
+                "Either mail_address/password or refresh_token is required."
+            )
+        if (self._mail_address != "") and ("@" not in self._mail_address):
+            raise ValueError("mail_address must contain '@' charactor.")
+
+    def _is_colab(self) -> bool:
+        """
+        Return True if running in colab
+        """
+        return "google.colab" in sys.modules
+
+    def _load_config(self) -> dict:
+        """
+        load config from files and environment variables
+
+        Args:
+            N/A
+        Returns:
+            dict: configurations
+        """
+        config: dict = {}
+
+        # colab config
+        if self._is_colab():
+            colab_config_path = (
+                "/content/drive/MyDrive/drive_ws/secret/jquants-api.toml"
+            )
+            config = {**config, **self._read_config(colab_config_path)}
+
+        # user default config
+        user_config_path = f"{Path.home()}/.jquants-api/jquants-api.toml"
+        config = {**config, **self._read_config(user_config_path)}
+
+        # current dir config
+        current_config_path = "jquants-api.toml"
+        config = {**config, **self._read_config(current_config_path)}
+
+        # env specified config
+        if "JQUANTS_API_CLIENT_CONFIG_FILE" in os.environ:
+            env_config_path = os.environ["JQUANTS_API_CLIENT_CONFIG_FILE"]
+            config = {**config, **self._read_config(env_config_path)}
+
+        # env vars
+        config["mail_address"] = os.environ.get(
+            "JQUANTS_API_MAIL_ADDRESS", config.get("mail_address", "")
+        )
+        config["password"] = os.environ.get(
+            "JQUANTS_API_PASSWORD", config.get("password", "")
+        )
+        config["refresh_token"] = os.environ.get(
+            "JQUANTS_API_REFRESH_TOKEN", config.get("refresh_token", "")
+        )
+
+        return config
+
+    def _read_config(self, config_path: str) -> dict:
+        """
+        read config from a toml file
+
+        Params:
+            config_path: a path to a toml file
+        """
+        if not os.path.isfile(config_path):
+            return {}
+
+        with open(config_path, mode="rb") as f:
+            ret = tomllib.load(f)
+
+        if "jquants-api-client" not in ret:
+            return {}
+
+        return ret["jquants-api-client"]
 
     def _base_headers(self) -> dict:
         """
         J-Quants API にアクセスする際にヘッダーにIDトークンを設定
         """
-        headers = {"Authorization": f"Bearer {self.get_id_token()}"}
+        id_token = self.get_id_token()
+        headers = {"Authorization": f"Bearer {id_token}"}
         return headers
 
     def _request_session(
         self,
         status_forcelist: Optional[List[int]] = None,
         method_whitelist: Optional[List[str]] = None,
-    ):
+    ) -> requests.Session:
         """
         requests の session 取得
 
@@ -76,7 +203,7 @@ class Client:
 
         return self._session
 
-    def _get(self, url: str, params: dict = None) -> requests.Response:
+    def _get(self, url: str, params: Optional[dict] = None) -> requests.Response:
         """
         requests の get 用ラッパー
 
@@ -98,7 +225,11 @@ class Client:
         return ret
 
     def _post(
-        self, url: str, payload: dict = None, headers: dict = None
+        self,
+        url: str,
+        data: Optional[_Data] = None,
+        json: Optional[Any] = None,
+        headers: Optional[dict] = None,
     ) -> requests.Response:
         """
         requests の get 用ラッパー
@@ -116,16 +247,92 @@ class Client:
         """
         s = self._request_session()
 
-        ret = s.post(url, data=payload, headers=headers, timeout=30)
+        ret = s.post(url, data=data, json=json, headers=headers, timeout=30)
         ret.raise_for_status()
         return ret
 
-    def get_id_token(self) -> str:
+    def get_refresh_token(
+        self, mail_address: Optional[str] = None, password: Optional[str] = None
+    ) -> str:
+        """
+        get J-Quants API refresh token
+
+        Params:
+            mail_address: J-Quants API login email address
+            password: J-Quants API login password
+        Returns:
+            refresh_token: J-Quants API refresh token
+        """
+        if self._refresh_token_expire > pd.Timestamp.utcnow():
+            return self._refresh_token
+
+        if mail_address is None:
+            mail_address = self._mail_address
+        if password is None:
+            password = self._password
+
+        if mail_address == "" or password == "":
+            raise ValueError("mail_address/password are required")
+        if (mail_address is not None) and ("@" not in mail_address):
+            raise ValueError("mail_address must contain '@' charactor.")
+
+        url = f"{self.JQUANTS_API_BASE}/token/auth_user"
+        data = {
+            "mailaddress": mail_address,
+            "password": password,
+        }
+        ret = self._post(url, json=data)
+        refresh_token = ret.json()["refreshToken"]
+        self._refresh_token = refresh_token
+        self._refresh_token_expire = pd.Timestamp.utcnow() + pd.Timedelta(6, unit="D")
+        return self._refresh_token
+
+    @retry(
+        retry=retry_if_exception_type(TokenAuthRefreshBadRequestException),
+        stop=stop_after_attempt(3),
+        wait=wait_exponential(multiplier=1, min=5, max=300),
+    )
+    def get_id_token(self, refresh_token: Optional[str] = None) -> str:
+        """
+        get J-Quants API id_token
+
+        Params:
+            refresh_token: J-Quants API refresh token
+        Retruns:
+            id_token: J-Quants API id token
+        """
         if self._id_token_expire > pd.Timestamp.utcnow():
             return self._id_token
 
-        url = f"{self.JQUANTS_API_BASE}/token/auth_refresh?refreshtoken={self.refresh_token}"
-        ret = self._post(url)
+        if refresh_token is not None:
+            _refresh_token = refresh_token
+        else:
+            _refresh_token = self.get_refresh_token()
+
+        url = (
+            f"{self.JQUANTS_API_BASE}/token/auth_refresh?refreshtoken={_refresh_token}"
+        )
+        try:
+            ret = self._post(url)
+        except HTTPError as e:
+            # retry if:
+            # - refresh_token is not provided as a parameter
+            # - error is 400 bad request
+            # - mail_address and password are provided
+            if (
+                refresh_token is None
+                and e.response.status_code == 400
+                and self._mail_address != ""
+                and self._password != ""
+            ):
+                # clear tokens for the next try
+                self._refresh_token = ""
+                self._refresh_token_expire = pd.Timestamp.utcnow()
+                self._id_token = ""
+                self._id_token_expire = pd.Timestamp.utcnow()
+                # raise for retrying
+                raise TokenAuthRefreshBadRequestException(e)
+            raise e
         id_token = ret.json()["idToken"]
         self._id_token = id_token
         self._id_token_expire = pd.Timestamp.utcnow() + pd.Timedelta(23, unit="hour")
@@ -174,7 +381,7 @@ class Client:
             pd.DataFrame: セクター一覧
         """
         url = f"{self.JQUANTS_API_BASE}/listed/sections"
-        params: Dict = {}
+        params: dict = {}
         ret = self._get(url, params)
         d = ret.json()
         df = pd.DataFrame.from_dict(d["sections"])
@@ -305,6 +512,9 @@ class Client:
         Returns:
             pd.DataFrame: 株価情報 (Code, Date列でソートされています)
         """
+        # pre-load id_token
+        self.get_id_token()
+
         buff = []
         dates = pd.date_range(start_dt, end_dt, freq="D")
         with ThreadPoolExecutor(max_workers=self.MAX_WORKERS) as executor:
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,236 @@
+from contextlib import nullcontext as does_not_raise
 from datetime import datetime
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 
 import pandas as pd
+import pytest
 from dateutil import tz
 
 import jquantsapi
+
+
+@pytest.mark.parametrize(
+    "mail_address, password, refresh_token,"
+    "env, isfile, load,"
+    "exp_mail_address, exp_password, exp_refresh_token, exp_refresh_token_expire,"
+    "exp_raise",
+    (
+        (
+            None,
+            None,
+            None,
+            {},
+            [True, False, True, False],
+            [{"dummy": {"mail_address": "mail_address"}}, {}],
+            "",
+            "",
+            "",
+            0,
+            pytest.raises(ValueError),
+        ),
+        (
+            None,
+            None,
+            None,
+            {"JQUANTS_API_CLIENT_CONFIG_FILE": ""},
+            [True, True, True, True],
+            [
+                {
+                    "jquants-api-client": {
+                        "mail_address": "mail_address@",
+                        "password": "password",
+                    }
+                },
+                {},
+                {},
+                {
+                    "jquants-api-client": {
+                        "mail_address": "mail_address_overwrite@",
+                        "refresh_token": "refresh_token",
+                    }
+                },
+            ],
+            "mail_address_overwrite@",
+            "password",
+            "refresh_token",
+            6,
+            does_not_raise(),
+        ),
+        (
+            None,
+            None,
+            None,
+            {
+                "JQUANTS_API_MAIL_ADDRESS": "mail_address_env@",
+                "JQUANTS_API_PASSWORD": "password_env",
+                "JQUANTS_API_REFRESH_TOKEN": "refresh_token_env",
+            },
+            [True, False, False, False],
+            [
+                {
+                    "jquants-api-client": {
+                        "mail_address": "mail_address@",
+                        "password": "password",
+                        "refresh_token": "refresh_token",
+                    }
+                },
+            ],
+            "mail_address_env@",
+            "password_env",
+            "refresh_token_env",
+            6,
+            does_not_raise(),
+        ),
+        (
+            "mail@",
+            "password",
+            None,
+            {},
+            [False, False, False, False],
+            [],
+            "mail@",
+            "password",
+            "",
+            0,
+            does_not_raise(),
+        ),
+        (
+            "mail@",
+            None,
+            None,
+            {},
+            [False, False, False, False],
+            [],
+            "mail@",
+            "",
+            "",
+            0,
+            pytest.raises(ValueError),
+        ),
+        (
+            "mail",
+            "password",
+            None,
+            {},
+            [False, False, False, False],
+            [],
+            "mail",
+            "password",
+            "",
+            0,
+            pytest.raises(ValueError),
+        ),
+        (
+            None,
+            None,
+            "token",
+            {},
+            [False, False, False, False],
+            [],
+            "",
+            "",
+            "token",
+            6,
+            does_not_raise(),
+        ),
+        (
+            "mail_address_param@",
+            "password_param",
+            "refresh_token_param",
+            {},
+            [True, False, False, False],
+            [
+                {
+                    "jquants-api-client": {
+                        "mail_address": "mail_address@",
+                        "password": "password",
+                        "refresh_token": "refresh_token",
+                    }
+                },
+            ],
+            "mail_address_param@",
+            "password_param",
+            "refresh_token_param",
+            6,
+            does_not_raise(),
+        ),
+    ),
+)
+def test_client(
+    mail_address,
+    password,
+    refresh_token,
+    env,
+    isfile,
+    load,
+    exp_mail_address,
+    exp_password,
+    exp_refresh_token,
+    exp_refresh_token_expire,
+    exp_raise,
+):
+    utcnow = pd.Timestamp("2022-09-08T22:00:00Z")
+    with exp_raise, patch.object(
+        jquantsapi.Client, "_is_colab", return_value=True
+    ), patch.object(jquantsapi.client.os.path, "isfile", side_effect=isfile), patch(
+        "builtins.open"
+    ), patch.dict(
+        jquantsapi.client.os.environ, env, clear=True
+    ), patch.object(
+        jquantsapi.client.tomllib, "load", side_effect=load
+    ), patch.object(
+        jquantsapi.client.pd.Timestamp, "utcnow", return_value=utcnow
+    ):
+        cli = jquantsapi.Client(
+            refresh_token=refresh_token, mail_address=mail_address, password=password
+        )
+        assert cli._mail_address == exp_mail_address
+        assert cli._password == exp_password
+        assert cli._refresh_token == exp_refresh_token
+        assert cli._refresh_token_expire == utcnow + pd.Timedelta(
+            exp_refresh_token_expire, unit="D"
+        )
+
+
+@pytest.mark.parametrize(
+    "init_mail_address, init_password, param_mail_address, param_password, exp_raise",
+    (
+        # use private variable via constructor
+        ("m@", "p", None, None, does_not_raise()),
+        ("", "", None, None, pytest.raises(ValueError)),
+        ("m", "p", None, None, pytest.raises(ValueError)),
+        # use parameter
+        (None, None, "m@", "p", does_not_raise()),
+        (None, None, "", "", pytest.raises(ValueError)),
+        (None, None, "m", "p", pytest.raises(ValueError)),
+        # overwrite
+        ("m@", "p", "", "", pytest.raises(ValueError)),
+        ("m@", "p", "m", "p", pytest.raises(ValueError)),
+    ),
+)
+def test_get_refresh_token(
+    init_mail_address, init_password, param_mail_address, param_password, exp_raise
+):
+    config = {
+        "mail_address": "",
+        "password": "",
+        "refresh_token": "dummy_token",
+    }
+
+    with exp_raise, patch.object(
+        jquantsapi.Client, "_load_config", return_value=config
+    ), patch.object(jquantsapi.Client, "_post") as mock_post:
+        mock_post.return_value.json.return_value = {"refreshToken": "ret_token"}
+
+        cli = jquantsapi.Client(
+            refresh_token="dummy_token",
+            mail_address=init_mail_address,
+            password=init_password,
+        )
+        # overwrite expire time
+        cli._refresh_token_expire = pd.Timestamp.utcnow()
+        ret = cli.get_refresh_token(param_mail_address, param_password)
+        assert ret == "ret_token"
 
 
 def test_get_price_range():
@@ -12,10 +238,12 @@ def test_get_price_range():
     get_price_range()を呼ぶ際、引数に様々な型が入っていても問題なく
     get_prices_daily_quotes()に単一の年月日が渡される事を確認する。
     """
-    # テストではHTTP通信を行わず、get_prices_daily_quotes()をモックすることで、
+    # テストではHTTP通信を行わず、
+    # get_id_token()およびget_prices_daily_quotes()をモックすることで、
     # 実際に通信が発生しないようにする。
     mock = MagicMock(return_value=pd.DataFrame(columns=["Code", "Date"]))
     cli = jquantsapi.Client(refresh_token="dummy")
+    cli.get_id_token = MagicMock()
     cli.get_prices_daily_quotes = mock  # get_prices_daily_quotes() をモックに置き換える
     formats = {
         # テストする期間はいつでも良いので、何かが起こりやすい閏日をテスト対象に含む
