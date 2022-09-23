557287f3c0304369eaecae21a866fda26c7d99c0
s-meitoma <74633813+s-meitoma@users.noreply.github.com>
2022-09-23 13:25:24 +0900

    Correct Typo jquantsapi/enums.py

--- jquantsapi/enums.py
+++ jquantsapi/enums.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class MARKET_API_SECTIONS(str, Enum):
     """
-    values of sections for marekt api
+    values of sections for market api
 
     ref. (en) https://jpx.gitbook.io/j-quants-api-en/api-reference/markets-api/section-code
     ref. (ja) https://jpx.gitbook.io/j-quants-api/api-reference/market-api/sector_name
